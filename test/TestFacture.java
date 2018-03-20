3bf1ecd4d36e47c1cf27d36ebfb1ef0f9fa1039f
Émile Brunelle <emilebrunelle@icloud.com>
2018-03-19 22:53:06 -0400

    Nettoyage des imports non utilisés

--- test/TestFacture.java
+++ test/TestFacture.java
@@ -5,9 +5,7 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import main.Client;
