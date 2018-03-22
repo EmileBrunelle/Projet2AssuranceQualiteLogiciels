196ea3a2607b95e38b5a798468d394ce080d11fe
Émile Brunelle <ebrunelle08@gmail.com>
2018-03-22 13:47:56 -0400

    Ajout du test des plats et modification des tests de client

---
+++ test/TestClient.java
@@ -0,0 +1,85 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Émile
+ *
+ */
+public class TestClient {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link main.Client#Client(java.lang.String)}.
+	 */
+	@Test
+	public void testClient() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link main.Client#setMontantFacture(double)}.
+	 */
+	@Test
+	public void testSetMontantFacture() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link main.Client#getNom()}.
+	 */
+	@Test
+	public void testGetNom() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link main.Client#getMontantFacture()}.
+	 */
+	@Test
+	public void testGetMontantFacture() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link main.Client#toString()}.
+	 */
+	@Test
+	public void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
