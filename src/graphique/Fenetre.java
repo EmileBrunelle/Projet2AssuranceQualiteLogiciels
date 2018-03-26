7c3a57ea910a3cdcd8b62a64c0161d15bc478a61
Émile Brunelle <ebrunelle08@gmail.com>
2018-03-26 09:41:20 -0400

    Ajout du début de l'interface graphique

---
+++ src/graphique/Fenetre.java
@@ -0,0 +1,13 @@
+package graphique;
+
+import javax.swing.JFrame;
+
+public class Fenetre extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+	public Fenetre() {
+		super("Facture");
+		
+	}
+
+}
---
+++ src/graphique/ZoneFacture.java
@@ -0,0 +1,12 @@
+package graphique;
+
+import javax.swing.JTextArea;
+
+public class ZoneFacture extends JTextArea {
+	private static final long serialVersionUID = 1L;
+
+	public ZoneFacture() {
+		
+	}
+
+}
